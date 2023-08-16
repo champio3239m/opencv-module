e0a98edbe78aa0d2859bbac169d50ecd5973f959
Devarsh <140988423+champio3239m@users.noreply.github.com>
2023-08-16 18:10:20 +0530

    face_recog_m.py

--- face_recognition.py
+++ face_recognition.py
@@ -39,14 +39,14 @@ def recognise_face():
      people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
      face_recognizer = cv.face.LBPHFaceRecognizer_create()
      face_recognizer.read('face_trained.yml')
-     img = cv.imread(r'Faces/val/elton_john/4.jpg')
+     img = cv.imread(r'Faces/val/ben_afflek/3.jpg')
      gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
      cv.imshow('Person', gray)
      faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
      for (x,y,w,h) in faces_rect:
        faces_roi = gray[y:y+h,x:x+w]
      label, confidence = face_recognizer.predict(faces_roi)
-     print(f'Label = {people[label]} with a confidence of {confidence}')
+     print(f'The person is {people[label]} with an estimated accuracy of {confidence}')
      cv.putText(img, str(people[label]), (20,20), cv.FONT_ITALIC, 1.0, (0,255,0), thickness=2)
      cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
      cv.imshow('Detected Face', img)
