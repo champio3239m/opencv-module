f75954f2fb99324499f79b3120fd24009b7967c4
Devarsh <140988423+champio3239m@users.noreply.github.com>
2023-09-08 16:54:10 +0530

    module_all_functions.py

--- module_all_functions.py
+++ module_all_functions.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def oppening_an_image():
     img=cv.imread('Photos/cat.jpg')
@@ -290,7 +291,6 @@ def masking():
      cv.waitKey(0)
 
 def histogram_computation_gray():
-     import matplotlib.pyplot as plt
      img = cv.imread('Photos/cats.jpg')
      cv.imshow('cats', img)
      gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -307,7 +307,6 @@ def histogram_computation_gray():
      cv.waitKey(0)
 
 def histogram_computation_bgr():
-     import matplotlib.pyplot as plt
      img = cv.imread('Photos/cats.jpg')
      cv.imshow('Cats', img)
      blank = np.zeros(img.shape[:2], dtype='uint8')
